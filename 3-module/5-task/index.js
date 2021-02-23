1b22628cc06ea6d5f7bf99f2ac3025f24fc0ff5e
Garlex <info@garlex.ru>
2021-02-23 23:19:19 +0300

    3-module 5-task replace parse string

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,14 +1,9 @@
 function getMinMax(str) {
-  // '1, -5.8 или 10, хотя 34 + -5.3 и 73'
-  // '25,-1,-234,4,   1000'
-  let result = [];
-
-  str.split(',').forEach((item) => {
-    result.push(...item.split(' '));
-  });
-
-  result = result.map((item) => parseFloat(item))
-                 .filter((item) => isFinite(item));
+  let result = str.split(' ')
+    .join()
+    .split(',')
+    .map((item) => parseFloat(item))
+    .filter((item) => isFinite(item));
 
   return {
     min: Math.min(...result),
